fbd3ec676fa6effcc79daa88f0ee7c23e018c32a
Lisa Lee <snickers495@gmail.com>
2017-11-03 18:55:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,13 +7,13 @@ class App < Sinatra::Base
   end
 
   post "/team" do
-    @team_name = params[:name]
+    @name = params[:name]
     @coach = params[:coach]
-    @point_guard = params[:pg]
-    @shooting_guard = params[:sg]
-    @small_forward = params[:sf]
-    @power_forward = params[:pf]
-    @center = params[:c]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
     erb :team
   end
 
